710af7d154bf177eaee0f9a6ed4b4a0296a95a32
krishanu009 <krishanudeb58@gmail.com>
2023-11-14 18:59:55 +0530

    content display on date selection logic

--- src/components/calenderView.js
+++ src/components/calenderView.js
@@ -6,14 +6,34 @@ import 'react-calendar/dist/Calendar.css';
 import '../styling/calender.css'
 
 const CalendarView = () =>  {
+  let emptyDispObj = {date:"", title:"", Link:""};
     const [date, setDate] = useState(new Date());
+    const [displayedInfo,setDisplayedInfo] = useState(emptyDispObj );
+    
     useEffect(() => {
-        fetchInfoOnDate(date);
+        fetchInfoOnDate(date.toISOString());
       }, [date]);
+    const  scheduledContent = [{date:"2023-11-02T18:30:00.000Z", title:"El dorado Mystery!", Link:"url//.com"},
+    {date:"2023-11-04T18:30:00.000Z", title:"Atlentis Mytery!", Link:"url//.com"},
+    {date:"2023-11-06T18:30:00.000Z", title:"Kerla Mystery!", Link:"url//.com"}];
 
       function fetchInfoOnDate(date)
       {
-         console.log(date);
+        console.log(date);
+        let foundObj = scheduledContent.find((item) => {
+          return item.date === date;
+        });
+
+        if(foundObj)
+        {
+           setDisplayedInfo(foundObj);
+        }
+        else
+        {
+          emptyDispObj.date = date;
+          setDisplayedInfo(emptyDispObj )
+        }
+        console.log(foundObj);
       }
   return (
     <>
@@ -22,7 +42,7 @@ const CalendarView = () =>  {
       <Row>
         <Col lg="6">
             <div className='dateInfo'>
-                
+                <h1>Title: {displayedInfo.title}</h1>
             </div>
         </Col>
         <Col>
@@ -34,6 +54,13 @@ const CalendarView = () =>  {
         <span className='bold'>Selected Date:</span>{' '}
         {date.toDateString()}
       </p></Col>
+      </Row>
+      <Row>
+        <Col >
+        <div className='notScheduled'>
+                
+                </div>
+        </Col>
       </Row>
      
       </>
